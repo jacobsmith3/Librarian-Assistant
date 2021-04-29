184ecfdd864f92f4ac94566269e2f7462f1f4791
tayho <78052529+Wave99-prog@users.noreply.github.com>
2021-04-29 16:13:04 -0700

    Latest update

--- UserProfile.cpp
+++ UserProfile.cpp
@@ -7,39 +7,51 @@ class UserProfile {
 		// User Login
 		string username = "";
 		string password = "";
-		string accountStatus = "Locked";
+		string accountStatus ;
 		int32_t accountBalance = 0 ;
 
+	// display user profile
 	void viewProfile() {
 		cout << username << endl;
 		cout << accountStatus << endl;
 		cout << accountBalance << endl;
 	}
-	// change password on database
-	void editPass(string newPassword) {
-		password = newPassword;
-	}
+
 	// change username on database
 	void editUser(string newUsername) {
 		username = newUsername;
 	}
-	// the account balance minus the amount 
-	void payBalance(int amount) {
-		accountBalance = accountBalance - amount;
+	
+	// change password on database
+	void editPass(string newPassword) {
+		password = newPassword;
 	}
+	
+	// assign account stauts Admin or User
+	void setAccountStatus(string status) {
+		if (status == "Admin") {
+			accountStatus = status;
+		}
+		else accountStatus = "User";
+	}
+
 	// set the account balance 
 	void setBalance(int amount) {
 		accountBalance = amount;
 	}
 
+	// the account balance minus the amount 
+	void payBalance(int amount) {
+		accountBalance = accountBalance - amount;
+	}
+
+	// get method for User Profile class
 	string getUser() {
 		return username;
 	}
-
 	string getStatus() {
 		return accountStatus;
 	}
-
 	int32_t getBalance() {
 		return accountBalance;
 	}
