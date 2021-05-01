965750533aff1555ed7ec0bcaf8025c5dbfa67d3
jacobsmith3 <63427236+jacobsmith3@users.noreply.github.com>
2021-04-30 20:04:28 -0700

    Update Checkout.cpp

--- Checkout.cpp
+++ Checkout.cpp
@@ -1,30 +1,67 @@
 #include <iostream>
 #include <list>
+#include <iterator>
 #include "UserProfile.cpp"
 #include "BookProfile.cpp"
+#include ".cpp"
+
 using namespace std;
 
 class Checkout {
     private:
-    list:<BookProfile> Books  //this will be an array with BookProfile data types inside
-    User = UserProfile() 
+    list:<BookProfile> Books;  //this will be an array with BookProfile data types inside
+    User = UserProfile();
+    lookup = Search();
     
     public:
     bool BookLimit()
     {
         if(Books.size()<=3)
         {
-            return true
+            return true;
         }
         else
         {
-            return false
+            cout<<"Cant checkout more than three books"<<endl;
+            return false;
         }
     }
     
+    void setSearch(Search set)
+    {
+        lookup = set;
+    }
+    
+    void printBooks()
+    {
+        list<BookProfile> :: iterator it;
+        for(it = Books.begins(); it != Books.end();++it)
+        {
+            *it.bookProfile();
+            cout<<endl;
+        }
+    }
     void checkout()
     {
-        for int i = 0:
-        
+        if((User.getBalance()-(2.5*Books.size())) < 0)
+        {
+            cout<<"Not enought funds";
+            return;
+        }
+        list<BookProfile> :: iterator it;
+        for(it = Books.begins(); it != Books.end();++it)
+        {
+            int temp = *it.getAvailability();
+            if(temp == 0)
+            {
+                cout<<"None in stock"<<endl;
+            }
+            BookProfile holder = lookup.find(*it.bookISBN);
+            holder.setAvailability(temp-1);
+            lookup.erase(temp);
+            lookup.inserrt(holder.bookISBN, holder);
+        }
+        cout<<"thank you, enjoy the books!"<<endl;    
+       
     }
 };
