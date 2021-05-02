4060794b1107fef3167a9f8c792ccd6eac9135ef
austinmartin55 <63477587+austinmartin55@users.noreply.github.com>
2021-05-01 17:43:07 -0700

    Updated 5/1/21

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
+#include "UserProfile.cpp"
+#include "BookProfile.cpp"
+#include "Search.cpp"
+#include "Checkout.cpp"
+
 int main()
 {
-
+    Search S;
+    ifstream infile;
+    infile.open("Books.txt", ios::in);
+    S.initBookList(infile);
     return 0;
 }
